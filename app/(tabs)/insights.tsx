abbe9c95e7a354fc0216debda7434e6e9fbef9ca
nikithreddy <kadapalanikith@gmail.com>
2026-03-31 15:56:51 +0530

    first commit

---
+++ app/(tabs)/insights.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+
+const Insights = () => {
+    return (
+        <View>
+            <Text>Insights</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Insights;
---
+++ app/(tabs)/subscriptions.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View,Text } from 'react-native';
+
+const Subscriptions = () => {
+    return (
+        <View>
+            <Text>Subscriptions</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Subscriptions;
