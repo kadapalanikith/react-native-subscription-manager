68ece72d83f13e9e4e037b627ae294faf7ac0e38
nikithreddy <kadapalanikith@gmail.com>
2026-04-02 11:18:12 +0530

    implemented fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,24 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
-import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
 import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 const SafeAreaView = styled(RNSafeAreaView);
 
-
-export default function HomeScreen() {
+export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">Home Screen</Text>
+    <SafeAreaView className="flex-1 p-5 bg-background">
+      <Text className="text-5xl font-sans-extrabold">Home</Text>
       <Link
         href="/onboarding"
-        className="mt-4 rounded bg-primary p-4 text-background"
+        className="mt-4 rounded bg-primary font-sans-bold p-4 text-white"
       >
         Go to Onboarding
       </Link>
-      <Link href="/sign-in" className="mt-4 rounded bg-accent p-4 text-primary">
+      <Link href="/sign-in" className="mt-4 rounded bg-primary text-white font-sans-bold p-4 ">
         Sign In
       </Link>
-      <Link href="/sign-up" className="mt-4 rounded bg-accent p-4 text-primary">
+      <Link href="/sign-up" className="mt-4 rounded bg-primary text-white font-sans-bold p-4">
         Sign Up
-      </Link>
-
-      <Link href="/subscriptions/spotify">Spotify Sub</Link>
-      <Link
-        href={{
-          pathname: "/subscriptions",
-        }}
-      >
-        Apple Music Sub
       </Link>
     </SafeAreaView>
   );
